56381abc5d5c5514eb90465c630f498d146c9cce
Rachel Denton <rdenton3@terpmail.umd.edu>
2022-04-22 20:42:58 -0400

    user mutations defined

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -20,8 +20,29 @@ type Book {
     title: String
 }
 type Query {
+    me: User
     users: [User]
     user(username: String!): User
+  }
+type Auth {
+    token: ID!
+    user: User
+}
+
+input BookInput {
+    authors: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput): Book
+    removeBook(id: ID!,input: BookInput): User
   }
 `;
 
